c49fed6271d6ec85749f3e7faae49c907ed1975c
ekolpakov <ekolpakov@greet-go.com>
2018-11-09 16:11:37 +0600

    Made coll message

--- src/main/java/kz/greetgo/depinject_hello/beans/SomeBean.java
+++ src/main/java/kz/greetgo/depinject_hello/beans/SomeBean.java
@@ -5,6 +5,10 @@ import kz.greetgo.depinject.core.Bean;
 @Bean
 public class SomeBean {
   public void hello() {
-    System.out.println("Hello from SOME BEAN");
+    System.out.println();
+    System.out.println("\tThe application runs ok");
+    System.out.println();
+    System.out.println("\tHello from SOME BEAN");
+    System.out.println();
   }
 }
